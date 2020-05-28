322c5ebeea455de21e3048d3029e3a424d979d7c
AnupamaTK <swathathsarani@gmail.com>
2020-05-28 13:51:49 +0530

    Reseting Form

--- src/views/Ministry Store/RegisterMinistryStore/RegisterMinistryStore.js
+++ src/views/Ministry Store/RegisterMinistryStore/RegisterMinistryStore.js
@@ -31,8 +31,7 @@ class RegisterMinistryStore extends Component {
     location: '',
     total_storage:'',
     available_storage:''
-    // export_date: '',
-    // order_date: ''
+
   };
 
   constructor(props) {
@@ -80,8 +79,12 @@ class RegisterMinistryStore extends Component {
 
   toggleFade() {
     this.setState((prevState) => { return { fadeIn: !prevState } });
-  }
+ }
 
+ resetForm = () => { 
+   this.setState({ item: this.emptyItem});
+}
+ 
   render() {
     const {item} = this.state;
     const title = <h2>{'Add Group'}</h2>;
@@ -96,7 +99,7 @@ class RegisterMinistryStore extends Component {
                 <strong>Ministry Store Registration</strong>
               </CardHeader>
               <CardBody>
-                <Form onSubmit={this.handleSubmit} method="post" encType="multipart/form-data" className="form-horizontal">
+                <Form onSubmit={this.handleSubmit} method="post" encType="multipart/form-data" className="form-horizontal" id="ministryStoreForm">
                   {/*<FormGroup row>
                     <Col md="3">
                       <Label htmlFor="text-input">Ministry Store ID</Label>
@@ -111,7 +114,7 @@ class RegisterMinistryStore extends Component {
                       <Label htmlFor="text-input">Location</Label>
                     </Col>
                     <Col xs="12" md="9">
-                      <Input type="text" id="location" name="location" placeholder="Location" value={item.location || ''}
+                      <Input type="text" id="location" name="location" placeholder="Location"  initialValue="" value={item.location || ''}
                         onChange={this.handleChange} autoComplete="location" />
                     </Col>
                   </FormGroup>
@@ -121,7 +124,7 @@ class RegisterMinistryStore extends Component {
                       <Label htmlFor="text-input">Total Storage</Label>
                     </Col>
                     <Col xs="12" md="9">
-                      <Input type="text" id="total_storage" name="total_storage" placeholder="Total Storage" value={item.total_storage || ''}
+                      <Input type="text" id="total_storage" name="total_storage" placeholder="Total Storage" initialValue="" value={item.total_storage || ''}
                         onChange={this.handleChange} autoComplete="total_storage" />
                     </Col>
                   </FormGroup>
@@ -131,7 +134,7 @@ class RegisterMinistryStore extends Component {
                       <Label htmlFor="text-input">Avilable Storage</Label>
                     </Col>
                     <Col xs="12" md="9">
-                      <Input type="text" id="available_storage" name="available_storage" placeholder="Available Storage" value={item.available_storage || ''}
+                      <Input type="text" id="available_storage" name="available_storage" placeholder="Available Storage" initialValue="" value={item.available_storage || ''}
                         onChange={this.handleChange} autoComplete="available_storage" />
                     </Col>
                   </FormGroup>
@@ -155,7 +158,7 @@ class RegisterMinistryStore extends Component {
 
                   <FormGroup>
                     <Button type="submit" size="sm" color="primary"><i className="fa fa-dot-circle-o"></i> Submit</Button>{' '}
-                    <Button type="reset" size="sm" color="danger" tag={Link} to="/ministry_stores/ministry_stores_list"><i className="fa fa-ban"></i> Reset</Button>
+                    <Button type="reset" size="sm" color="danger" onClick={this.resetForm}><i className="fa fa-ban"></i> Reset</Button>
                   </FormGroup>
 
                 </Form>
